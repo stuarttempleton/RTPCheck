c2ce90a08bf0123a62b148ba7d72bdfac9dd93b6
Stuart Templeton <bvrn007@gmail.com>
2023-05-28 11:32:14 -0600

    Initial commit

---
+++ RTPCheck/Program.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.Win32;
+using RealTimeProtection;
+
+
+Console.WriteLine("Protection: " + (RTP.Disabled() ? "OFF" : "ON"));
---
+++ RTPCheck/RTP.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+
+
+namespace RealTimeProtection;
+internal class RTP
+{
+    public static bool Disabled()
+    {
+        bool disabled = true;
+        string key = "DisableRealtimeMonitoring";
+        string path = @"SOFTWARE\Microsoft\Windows Defender\Real-Time Protection";
+
+        RegistryKey basekey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+        if (basekey != null)
+        {
+            var val = basekey.OpenSubKey(path).GetValue(key);
+            disabled = val == null ? false : ((Int32)val != 0);
+        }
+        return disabled;
+    }
+
+    public static bool Enabled()
+    {
+        return !Disabled();
+    }
+}
